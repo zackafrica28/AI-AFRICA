59c9b1fcc0b0049578fef2795844db3c4d22a525
zackafrica28 <zackawudumk@gmail.com>
2026-02-13 05:14:00 +0100

    Update page.tsx

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import React, { useState } from "react";
 import FluxNavbar from "@/components/ui/FluxNavbar";
 import HolographicCard from "@/components/ui/HolographicCard";
 import styles from "./media.module.css";
