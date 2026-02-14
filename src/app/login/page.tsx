7afbbcff5396e250cffaf1933010eb45bf57a0ca
zackafrica28 <zackawudumk@gmail.com>
2026-02-14 04:41:43 +0100

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, FormEvent } from "react";
+import { useState} from "react";
 import { useAuth } from "@/context/AuthContext";
 import styles from "./login.module.css";
 import FluxNavbar from "@/components/ui/FluxNavbar";
