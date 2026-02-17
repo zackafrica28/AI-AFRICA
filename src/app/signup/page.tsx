62ca730e531d3fe0094278412c909dc0caa82809
zackafrica28 <zackawudumk@gmail.com>
2026-02-17 02:25:09 +0100

    Update page.tsx

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "@/lib/firebase"; // Direct import for creation
